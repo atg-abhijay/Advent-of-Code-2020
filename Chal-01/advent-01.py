3d3aaf4ac399d9315375f3c479712c1023962f7e
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-22 00:48:29 -0500

    Unite and cleanup parts 1 and 2

--- Chal-01/advent-01.py
+++ Chal-01/advent-01.py
@@ -4,51 +4,46 @@ URL for challenge: https://adventofcode.com/2020/day/1
 
 from functools import reduce
 
-def part1():
+def process_input():
+    f = open("advent-01-input.txt")
+    amounts = []
+    for line in f.readlines():
+        amounts.append(int(line))
+
+    return amounts
+
+def part1(target_amount, amounts):
     """
     Calculate the inverse of the given amounts
     (2020 - amount). By creating sets for them
     and taking the intersection, the only amounts
     that will be left are x and (2020 - x).
     """
-    f = open("advent-01-input.txt")
-    amounts = set()
-    inverse_amounts = set()
-    for line in f.readlines():
-        amount = int(line)
-        amounts.add(amount)
-        inverse_amounts.add(2020-amount)
+    inverse_amounts = [target_amount - x for x in amounts]
+    pair_entries = (set(amounts)).intersection(set(inverse_amounts))
+    num_entries = len(pair_entries)
 
-    pair_entries = amounts.intersection(inverse_amounts)
-    if len(pair_entries) == 1:
+    if num_entries == 1:
         return pair_entries.pop() ** 2
 
-    return reduce(lambda x, y: x*y, pair_entries)
+    elif num_entries == 2:
+        return reduce(lambda x, y: x*y, pair_entries)
+
+    return None
 
 def part2():
-    f = open("advent-01-input.txt")
-    amounts = []
-    inverse_amounts = []
-    for line in f.readlines():
-        amount = int(line)
-        amounts.append(amount)
-        inverse_amounts.append(2020-amount)
+    amounts = process_input()
+    inverse_amounts = [2020 - x for x in amounts]
 
     for idx, inv in enumerate(inverse_amounts):
-        sub_amounts = set(amounts[:idx] + amounts[idx+1:])
-        inv_sub_amounts = set(inv - x for x in sub_amounts)
-        pair_entries = sub_amounts.intersection(inv_sub_amounts)
-        num_entries = len(pair_entries)
-        if num_entries == 1:
-            return amounts[idx] * (pair_entries.pop() ** 2)
-        elif num_entries == 2:
-            return amounts[idx] * (reduce(lambda x, y: x*y, pair_entries))
-
+        sub_solution = part1(inv, amounts[:idx] + amounts[idx+1:])
+        if sub_solution is not None:
+            return amounts[idx] * sub_solution
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
     if chall == 1:
-        print(part1())
+        print(part1(2020, process_input()))
     elif chall == 2:
         print(part2())
     else:
