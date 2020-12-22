3435a6e3b99e684910428bc335467945e89a7aa7
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-22 00:14:11 -0500

    Solve Part 2 (requires cleanup)

--- Chal-01/advent-01.py
+++ Chal-01/advent-01.py
@@ -26,7 +26,24 @@ def part1():
     return reduce(lambda x, y: x*y, pair_entries)
 
 def part2():
-    return
+    f = open("advent-01-input.txt")
+    amounts = []
+    inverse_amounts = []
+    for line in f.readlines():
+        amount = int(line)
+        amounts.append(amount)
+        inverse_amounts.append(2020-amount)
+
+    for idx, inv in enumerate(inverse_amounts):
+        sub_amounts = set(amounts[:idx] + amounts[idx+1:])
+        inv_sub_amounts = set(inv - x for x in sub_amounts)
+        pair_entries = sub_amounts.intersection(inv_sub_amounts)
+        num_entries = len(pair_entries)
+        if num_entries == 1:
+            return amounts[idx] * (pair_entries.pop() ** 2)
+        elif num_entries == 2:
+            return amounts[idx] * (reduce(lambda x, y: x*y, pair_entries))
+
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
