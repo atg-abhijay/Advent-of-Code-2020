336da91cd1338b57a55a2c30ec0f91e52dfca17f
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-22 20:05:40 -0500

    Solve Part 1

--- Day-03/advent-03.py
+++ Day-03/advent-03.py
@@ -3,7 +3,23 @@ URL for challenge: https://adventofcode.com/2020/day/3
 """
 
 def part1():
-    return
+    f = open("advent-03-input.txt")
+    grid = []
+    for row in f.readlines():
+        grid.append(list(row.strip()))
+
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+    current_row, current_col, num_trees = 0, 0, 0
+
+    while current_row < num_rows:
+        if grid[current_row][current_col] == '#':
+            num_trees += 1
+
+        current_col = (current_col + 3) % num_cols
+        current_row += 1
+
+    return num_trees
 
 def part2():
     return
