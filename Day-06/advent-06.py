33e19c12d23fd7f333f658ad65be16022025c0fc
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-23 21:34:44 -0500

    Solve Part 1

--- Day-06/advent-06.py
+++ Day-06/advent-06.py
@@ -4,7 +4,23 @@ URL for challenge: https://adventofcode.com/2020/day/6
 
 
 def part1():
-    return
+    f = open("advent-06-input.txt")
+    group_counts = []
+    yes_answers = set()
+    for person_answers in f.readlines():
+        if person_answers == '\n':
+            group_counts.append(len(yes_answers))
+            yes_answers.clear()
+            continue
+
+        person_answers = person_answers.strip()
+        yes_answers = yes_answers.union(set(person_answers))
+
+    group_counts.append(len(yes_answers))
+    return sum(group_counts)
+
+    group_counts.append(len(yes_answers))
+    return sum(group_counts)
 
 
 def part2():
