0750dbb0c471fb5fbbb74d8ef0682461f37d2ec2
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-24 20:42:00 -0500

    Run the instructions separately

--- Day-08/advent-08.py
+++ Day-08/advent-08.py
@@ -4,7 +4,7 @@ URL for challenge: https://adventofcode.com/2020/day/8
 
 
 def process_input():
-    f = open("/Users/AbhijayGupta/Projects/Advent-of-Code-2020/Day-08/advent-08-input.txt")
+    f = open("advent-08-input.txt")
     instructions = []
     for line in f.readlines():
         line = line.strip().split(sep=' ')
@@ -16,49 +16,13 @@ def process_input():
 
 def part1():
     instructions = process_input()
-    times_executed = [0 for x in range(len(instructions))]
-    accumulator, idx, should_stop = 0, 0, False
-    while not should_stop:
-        instr, amount = instructions[idx]
-        if times_executed[idx] > 0:
-            break
-
-        times_executed[idx] += 1
-        if instr == 'acc':
-            accumulator += amount
-            idx += 1
-
-        elif instr == 'jmp':
-            idx += amount
-
-        else:
-            idx += 1
-
+    accumulator = run_instructions(instructions, False)[0]
     return accumulator
 
 
 def part2():
     instructions = process_input()
-    times_executed = [0 for x in range(len(instructions))]
-    idx, should_stop = 0, False
-    corrupt_instr_idxs = []
-    corrupted_instr = []
-    while not should_stop:
-        instr, amount = instructions[idx]
-        if times_executed[idx] > 0:
-            break
-
-        times_executed[idx] += 1
-        if instr == 'acc':
-            idx += 1
-
-        elif instr == 'jmp':
-            corrupt_instr_idxs.append(idx)
-            idx += amount
-
-        else:
-            corrupt_instr_idxs.append(idx)
-            idx += 1
+    corrupt_instr_idxs = run_instructions(instructions, False)[1]
 
     for corrupt_idx in corrupt_instr_idxs:
         corrupt_instr = instructions[corrupt_idx][0]
@@ -67,27 +31,8 @@ def part2():
         else:
             instructions[corrupt_idx][0] = 'nop'
 
-        times_executed = [0 for x in range(len(instructions))]
-        accumulator, idx, should_stop = 0, 0, False
-        while not should_stop:
-            if idx >= len(instructions):
-                break
-
-            instr, amount = instructions[idx]
-            if times_executed[idx] > 0:
-                should_stop = True
-                break
-
-            times_executed[idx] += 1
-            if instr == 'acc':
-                accumulator += amount
-                idx += 1
-
-            elif instr == 'jmp':
-                idx += amount
-
-            else:
-                idx += 1
+        result = run_instructions(instructions, True)
+        accumulator, should_stop = result[0], result[2]
 
         if should_stop:
             instructions[corrupt_idx][0] = corrupt_instr
@@ -97,34 +42,39 @@ def part2():
     return accumulator
 
 
-    # if corrupted_instr[0] == 'jmp':
-    #     instructions[pred_idx][0] = 'nop'
-    # else:
-    #     instructions[pred_idx][0] = 'jmp'
+def run_instructions(instructions, check_termination):
+    num_instr = len(instructions)
+    times_executed = [0 for x in range(num_instr)]
+    accumulator, idx, should_stop = 0, 0, False
+    corrupt_instr_idxs = []
 
-    # accumulator, idx, should_stop = 0, 0, False
-    # num_instr = len(instructions)
-    # while not should_stop:
-    #     if idx >= num_instr:
-    #         break
+    while not should_stop:
+        if check_termination and idx >= num_instr:
+            break
 
-    #     instr, amount = instructions[idx]
-    #     if instr == 'acc':
-    #         accumulator += amount
-    #         idx += 1
+        instr, amount = instructions[idx]
+        if times_executed[idx] > 0:
+            should_stop = True
+            break
 
-    #     elif instr == 'jmp':
-    #         idx += amount
+        times_executed[idx] += 1
+        if instr == 'acc':
+            accumulator += amount
+            idx += 1
 
-    #     else:
-    #         idx += 1
+        elif instr == 'jmp':
+            corrupt_instr_idxs.append(idx)
+            idx += amount
 
-    # return accumulator
+        else:
+            corrupt_instr_idxs.append(idx)
+            idx += 1
+
+    return accumulator, corrupt_instr_idxs, should_stop
 
 
 def run():
     chall = int(input("Please enter either 1 or 2 for the challenges: "))
-    # chall = 2
     if chall == 1:
         print(part1())
     elif chall == 2:
