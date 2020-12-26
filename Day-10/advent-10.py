e3eefa9d83d9ed624ff7cf43c3f7edbe30683516
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-25 20:35:21 -0500

    Solve Part 1

--- Day-10/advent-10.py
+++ Day-10/advent-10.py
@@ -4,7 +4,39 @@ URL for challenge: https://adventofcode.com/2020/day/6
 
 
 def part1():
-    return
+    """
+    Since there is a way to use all adapters,
+    none of them will be excluded. Sort them
+    according to the joltages.
+
+    Since all of them are used, all adapters
+    will be within 3 joltages of each other and
+    the sorting will help ensure that the next
+    closest adapter is picked.
+    """
+    f = open("advent-10-input.txt")
+    joltages = []
+    for jolt in f.readlines():
+        joltages.append(int(jolt.strip()))
+
+    sorted_joltages = sorted(joltages)
+    # Add device's built-in adapter
+    sorted_joltages.append(sorted_joltages[-1] + 3)
+    one_jolt_diffs, three_jolt_diffs = 0, 0
+    # Charging outlet
+    prev_jolt = 0
+
+    for jolt in sorted_joltages:
+        jolt_diff = jolt - prev_jolt
+        if jolt_diff == 1:
+            one_jolt_diffs += 1
+
+        elif jolt_diff == 3:
+            three_jolt_diffs += 1
+
+        prev_jolt = jolt
+
+    return one_jolt_diffs * three_jolt_diffs
 
 
 def part2():
