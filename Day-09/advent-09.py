f870eef694bcbfae8b4305f94c6d99ea0d349d56
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-29 20:50:52 -0500

    Solve Part 1

--- Day-09/advent-09.py
+++ Day-09/advent-09.py
@@ -4,7 +4,32 @@ URL for challenge: https://adventofcode.com/2020/day/9
 
 
 def part1():
-    return
+    f = open("advent-09-input.txt")
+    input_nums = []
+    for line in f.readlines():
+        input_nums.append(int(line.strip()))
+
+    preamble_size = 25
+    preamble = set(input_nums[:preamble_size])
+    # print(input_nums)
+    # print(preamble)
+
+    idx_to_remove = -1
+    target_num = 0
+    for num in input_nums[preamble_size:]:
+        if idx_to_remove != -1:
+            preamble.remove(input_nums[idx_to_remove])
+
+        complement_set = set(num - val for val in preamble)
+        intxn_size = len(preamble.intersection(complement_set))
+        if intxn_size < 2:
+            target_num = num
+            break
+
+        preamble.add(num)
+        idx_to_remove += 1
+
+    return target_num
 
 
 def part2():
