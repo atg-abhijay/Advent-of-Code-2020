5c0384899e504ab44c7418deef1a93d9bb1bc15f
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-30 17:30:07 -0500

    Solve Part 1

--- Day-13/advent-13.py
+++ Day-13/advent-13.py
@@ -3,8 +3,25 @@ URL for challenge: https://adventofcode.com/2020/day/13
 """
 
 
+from math import ceil, inf
+
+
 def part1():
-    return
+    f = open("advent-13-input.txt")
+    input_lines = f.readlines()
+    departure_estimate = int(input_lines[0].strip())
+    parsed_input = input_lines[1].strip().split(',')
+    bus_ids = [int(b_id) for b_id in parsed_input if b_id != 'x']
+
+    earliest_bus, smallest_diff = 0, inf
+    for b_id in bus_ids:
+        multiplicator = ceil(departure_estimate/b_id)
+        diff = b_id * multiplicator - departure_estimate
+        if diff < smallest_diff:
+            smallest_diff = diff
+            earliest_bus = b_id
+
+    return earliest_bus * smallest_diff
 
 
 def part2():
