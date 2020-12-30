f762a796f2f7355b83f4bd6a442dcb941ac0fbe8
Abhijay Gupta <atg.abhijay@gmail.com>
2020-12-29 21:35:02 -0500

    Solve Part 2

--- Day-09/advent-09.py
+++ Day-09/advent-09.py
@@ -41,7 +41,31 @@ def part1():
 
 
 def part2():
-    return
+    input_nums = process_input()
+    invalid_num = part1()
+    invalid_idx = input_nums.index(invalid_num)
+    sub_list = input_nums[:invalid_idx]
+    sub_list_size = len(sub_list)
+
+    current_idx, current_sum = 0, 0
+    start_idx, end_idx = 0, 0
+    target_list = []
+    while True and current_idx < sub_list_size:
+        if current_sum == invalid_num:
+            target_list = sub_list[start_idx:end_idx+1]
+            break
+
+        new_sum = current_sum + sub_list[current_idx]
+        while new_sum > invalid_num:
+            new_sum -= sub_list[start_idx]
+            start_idx += 1
+
+        current_sum = new_sum
+        end_idx = current_idx
+        current_idx += 1
+
+    sorted_targets = sorted(target_list)
+    return sorted_targets[0] + sorted_targets[-1]
 
 
 def run():
