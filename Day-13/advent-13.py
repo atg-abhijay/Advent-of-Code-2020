78baa131289071800f44bbf03d9bb718204aeb8a
Abhijay Gupta <atg.abhijay@gmail.com>
2021-01-01 19:37:31 -0500

    Solve Part 2

--- Day-13/advent-13.py
+++ Day-13/advent-13.py
@@ -4,6 +4,7 @@ URL for challenge: https://adventofcode.com/2020/day/13
 
 
 from math import ceil, inf
+from functools import reduce
 
 
 def part1():
@@ -25,7 +26,55 @@ def part1():
 
 
 def part2():
-    return
+    f = open("advent-13-input.txt")
+    parsed_input = f.readlines()[1].strip().split(',')
+    buses, offsets = [], []
+    for idx, b_id in enumerate(parsed_input):
+        if b_id != 'x':
+            buses.append(int(b_id))
+            offsets.append(idx)
+
+    mod_values = [0]
+    for bus_id, offset in zip(buses[1:], offsets[1:]):
+        mod_values.append(bus_id - (offset % bus_id))
+
+    w_values = []
+    big_product = reduce(lambda x, y: x*y, buses)
+    for bus_id in buses:
+        z = int(big_product/bus_id)
+        y = find_modulo_inverse(z % bus_id, bus_id)
+        w_values.append(y * z)
+
+    solution = 0
+    for mod_val, w in zip(mod_values, w_values):
+        solution += (mod_val * w)
+
+    return solution % big_product
+
+
+def find_modulo_inverse(target, modulus):
+    """
+    Find modulo inverse x of target wrt modulus
+    such that target * x (mod modulus) == 1 using
+    Euclid's extended algorithm.
+    """
+    a0, a1 = 0, 1
+    b0, b1 = 1, 0
+    while target != 0:
+        quotient = modulus // target
+        remainder = modulus % target
+        modulus = target
+        target = remainder
+
+        a_tmp = a0 - quotient * a1
+        a0 = a1
+        a1 = a_tmp
+
+        b_tmp = b0 - quotient * b1
+        b0 = b1
+        b1 = b_tmp
+
+    return a0
 
 
 def run():
