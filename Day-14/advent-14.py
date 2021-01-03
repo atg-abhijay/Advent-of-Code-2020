f962e2a0c1a2a4fc003a2d41dfddc25a9d1eafb8
Abhijay Gupta <atg.abhijay@gmail.com>
2021-01-03 16:40:20 -0500

    Solve Part 1

--- Day-14/advent-14.py
+++ Day-14/advent-14.py
@@ -4,7 +4,28 @@ URL for challenge: https://adventofcode.com/2020/day/14
 
 
 def part1():
-    return
+    f = open("advent-14-input.txt")
+    instructions = []
+    for line in f.readlines():
+        fields = line.strip().split(' = ')
+        if fields[0] == "mask":
+            instructions.append(("mask", fields[1]))
+        else:
+            address = int(fields[0][4:-1])
+            instructions.append(("mem", address, int(fields[1])))
+
+    mask_with_zeroes, mask_with_ones = 0, 0
+    addresses = {}
+    for instr in instructions:
+        command = instr[0]
+        if command == "mask":
+            mask_with_zeroes = int(instr[1].replace('X', '0'), 2)
+            mask_with_ones = int(instr[1].replace('X', '1'), 2)
+        else:
+            value = instr[2]
+            addresses[instr[1]] = (value | mask_with_zeroes) & mask_with_ones
+
+    return sum(addresses.values())
 
 
 def part2():
