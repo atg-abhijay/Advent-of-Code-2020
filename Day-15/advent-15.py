0486de9e6520e394399df4a17f5136ba86423fff
Abhijay Gupta <atg.abhijay@gmail.com>
2021-01-04 19:05:30 -0500

    Solve Part 1

--- Day-15/advent-15.py
+++ Day-15/advent-15.py
@@ -4,7 +4,30 @@ URL for challenge: https://adventofcode.com/2020/day/15
 
 
 def part1():
-    return
+    f = open("advent-15-input.txt")
+    starting_nums = f.readline().strip().split(',')
+    starting_nums = [int(x) for x in starting_nums]
+    unique_nums = {}
+    # Add a dummy value so that the
+    # indices and turns match up
+    turn_vals = [-1]
+    for idx, num in enumerate(starting_nums):
+        unique_nums[num] = idx+1
+        turn_vals.append(num)
+
+    start_turn, stop_turn = len(starting_nums) + 1, 2020
+    for current_turn in range(start_turn, stop_turn+1):
+        prev_num = turn_vals[-1]
+        if prev_num not in unique_nums:
+            unique_nums[prev_num] = current_turn - 1
+            turn_vals.append(0)
+        else:
+            last_known_turn = unique_nums[prev_num]
+            turns_apart = (current_turn - 1) - last_known_turn
+            unique_nums[prev_num] = current_turn - 1
+            turn_vals.append(turns_apart)
+
+    return turn_vals[-1]
 
 
 def part2():
