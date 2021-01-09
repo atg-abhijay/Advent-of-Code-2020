f3d02fc36f960d467a20c113cb203a4504d05f96
Abhijay Gupta <atg.abhijay@gmail.com>
2021-01-08 20:33:01 -0500

    Add comments for Part 1

--- Day-17/advent-17.py
+++ Day-17/advent-17.py
@@ -7,6 +7,13 @@ from copy import deepcopy
 
 
 def part1():
+    """
+    Since all the cubes change their state
+    simultaneously, two pocket dimensions
+    will be required since the original
+    dimension cannot be modified whilst
+    the state transitions are happening.
+    """
     f = open("advent-17-input.txt")
     pocket_dimn = {0: {}}
     for row_idx, line in enumerate(f.readlines()):
@@ -19,15 +26,24 @@ def part1():
 
         pocket_dimn[0][row_idx] = row_dict
 
+    # Create a copy dimension to keep
+    # track of the state transitions
     pocket_dimn_after = deepcopy(pocket_dimn)
 
     for i in range(6):
+        # The state transitions will occur for the
+        # current input AS WELL AS the immediate
+        # neighbours. Add those neighbours.
         for z, layer in pocket_dimn.items():
             for x, row in layer.items():
                 for y, state in row.items():
                     neighbours = generate_neighbour_locations((z, x, y))
                     add_neighbours_to_dimn(neighbours, pocket_dimn_after)
 
+        # This dimension contains the current input
+        # and the immediate neighbours. Apply the
+        # state transitions for all of them with
+        # respect to the original dimension.
         for z, layer in pocket_dimn_after.items():
             for x, row in layer.items():
                 for y, state in row.items():
@@ -35,6 +51,8 @@ def part1():
                     pocket_dimn_after[z][x][y] = get_new_state(
                         state, neighbours, pocket_dimn)
 
+        # Copy over the modified dimension
+        # in preparation for the next iteration
         pocket_dimn = deepcopy(pocket_dimn_after)
 
     # Determine the number of active cubes
