61a86d19a8111b1c76bd0cbce8f924ea75a9b5a1
Abhijay Gupta <atg.abhijay@gmail.com>
2021-01-21 15:51:17 -0500

    Solve Part 1

--- Day-18/advent-18.py
+++ Day-18/advent-18.py
@@ -3,8 +3,76 @@ URL for challenge: https://adventofcode.com/2020/day/18
 """
 
 
+import string, re
+
+
+class Node(object):
+    def __init__(self, value, depth):
+        self.value = value
+        self.nesting_depth = depth
+        self.left_child = None
+        self.right_child = None
+
+
+def create_tree(expression, nesting_depth):
+    digits = set(string.digits)
+    current_node = Node('+', nesting_depth)
+    current_node.left_child = Node(0, nesting_depth)
+    idx = 0
+
+    while idx < len(expression):
+        element = expression[idx]
+
+        if set(element).issubset(digits):
+            node = Node(int(element), nesting_depth)
+            current_node.right_child = node
+
+        elif element in ['+', '*']:
+            node = Node(element, nesting_depth)
+            node.left_child = current_node
+            current_node = node
+
+        elif element == '(':
+            sub_result = create_tree(expression[idx+1:], nesting_depth+1)
+            current_node.right_child = sub_result[0]
+            idx += sub_result[1]
+
+        else:
+            return current_node, idx + 1
+
+        idx += 1
+
+    return current_node, idx
+
+
+def evaluate_tree(node):
+    if not node:
+        return 0
+
+    if not node.left_child and not node.right_child:
+        return node.value
+
+    if node.value == '+':
+        return evaluate_tree(node.left_child) + evaluate_tree(node.right_child)
+    elif node.value == '*':
+        return evaluate_tree(node.left_child) * evaluate_tree(node.right_child)
+
+
 def part1():
-    return
+    f = open("advent-18-input.txt")
+    total_sum = 0
+    for line in f.readlines():
+        line = line.strip().split(' ')
+        expression = []
+        for elem in line:
+            parsed_elem = re.split('([()])', elem)
+            expression += [pe for pe in parsed_elem if pe]
+
+        tree = create_tree(expression, 0)
+        output = evaluate_tree(tree[0])
+        total_sum += output
+
+    return total_sum
 
 
 def part2():
