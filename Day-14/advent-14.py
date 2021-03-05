d9b1925dd1ec580f5df5b3c3950293648cf4e590
Abhijay Gupta <atg.abhijay@gmail.com>
2021-03-05 12:46:50 -0500

    Solve Part 2

--- Day-14/advent-14.py
+++ Day-14/advent-14.py
@@ -34,7 +34,24 @@ def part1():
 
 
 def part2():
-    return
+    instructions = process_input()
+    addresses, float_idxs = {}, []
+    for instr in instructions:
+        command = instr[0]
+        if command == "mask":
+            float_idxs = [i for i, ch in enumerate(instr[1]) if ch == 'X']
+            num_floats = len(float_idxs)
+            mask_with_ones = int(instr[1].replace('X', '1'), 2)
+        else:
+            floating_addr = list(bin(instr[1] | mask_with_ones)[2:].zfill(36))
+            for x in range(2 ** num_floats):
+                x = bin(x)[2:].zfill(num_floats)
+                for bit, idx in zip(x, float_idxs):
+                    floating_addr[idx] = bit
+
+                addresses[int(''.join(floating_addr))] = instr[2]
+
+    return sum(addresses.values())
 
 
 def run():
