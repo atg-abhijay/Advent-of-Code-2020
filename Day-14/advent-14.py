3374a19b526c113369a502f3939c1b1ce1e3929a
Abhijay Gupta <atg.abhijay@gmail.com>
2021-03-05 15:29:16 -0500

    Solve Day 14: Docking Data (#17)
    Check PR description for working of solution.

---
+++ Day-14/advent-14.py
@@ -0,0 +1,67 @@
+"""
+URL for challenge: https://adventofcode.com/2020/day/14
+
+Check PR description for notes on solution.
+"""
+
+
+def process_input():
+    f = open("advent-14-input.txt")
+    instructions = []
+    for line in f.readlines():
+        fields = line.strip().split(' = ')
+        if fields[0] == "mask":
+            instructions.append(("mask", fields[1]))
+        else:
+            address = int(fields[0][4:-1])
+            instructions.append(("mem", address, int(fields[1])))
+
+    return instructions
+
+
+def part1():
+    instructions = process_input()
+    addresses = {}
+    for instr in instructions:
+        if instr[0] == "mask":
+            mask_with_zeroes = int(instr[1].replace('X', '0'), 2)
+            mask_with_ones = int(instr[1].replace('X', '1'), 2)
+        else:
+            addresses[instr[1]] = (instr[2] | mask_with_zeroes) & mask_with_ones
+
+    return sum(addresses.values())
+
+
+def part2():
+    instructions = process_input()
+    addresses, float_idxs = {}, []
+    num_bits = 36
+    for instr in instructions:
+        if instr[0] == "mask":
+            float_idxs = [i for i, ch in enumerate(instr[1]) if ch == 'X']
+            num_floats = len(float_idxs)
+            mask_with_ones = int(instr[1].replace('X', '1'), 2)
+        else:
+            floating_addr = list(bin(instr[1] | mask_with_ones)[2:].zfill(num_bits))
+            for x in range(2 ** num_floats):
+                x = bin(x)[2:].zfill(num_floats)
+                for bit, idx in zip(x, float_idxs):
+                    floating_addr[idx] = bit
+
+                addresses[int(''.join(floating_addr))] = instr[2]
+
+    return sum(addresses.values())
+
+
+def run():
+    chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    if chall == 1:
+        print(part1())
+    elif chall == 2:
+        print(part2())
+    else:
+        print("You need to enter either 1 or 2")
+        exit(1)
+
+
+run()
