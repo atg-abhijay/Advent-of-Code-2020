5d979dc20fe9147c0dc5db409a21a034fea8a721
Abhijay Gupta <atg.abhijay@gmail.com>
2021-03-09 13:12:04 -0500

    Do some optimization
    - Store the number of cards for both players outside the loop and
    then modify the stored values instead of calling len() multiple
    times inside the loop.
    
    - Do not need to use copy() since a *new* deque is created with the
    slice.

--- Day-22/advent-22.py
+++ Day-22/advent-22.py
@@ -4,7 +4,6 @@ URL for challenge: https://adventofcode.com/2020/day/22
 
 
 from collections import deque
-from copy import copy
 from itertools import islice
 
 
@@ -54,6 +53,7 @@ def calculate_score(winner_deck):
 
 def play_game(p1_cards, p2_cards):
     previous_configs = set()
+    p1_num_cards, p2_num_cards = len(p1_cards), len(p2_cards)
     while p1_cards and p2_cards:
         current_config = (tuple(p1_cards), tuple(p2_cards))
         if current_config in previous_configs:
@@ -61,17 +61,22 @@ def play_game(p1_cards, p2_cards):
 
         previous_configs.add(current_config)
         p1_top, p2_top = p1_cards.popleft(), p2_cards.popleft()
-        if len(p1_cards) >= p1_top and len(p2_cards) >= p2_top:
-            subgame_p1_cards = copy(deque(islice(p1_cards, p1_top)))
-            subgame_p2_cards = copy(deque(islice(p2_cards, p2_top)))
+        p1_num_cards -= 1
+        p2_num_cards -= 1
+
+        if p1_num_cards >= p1_top and p2_num_cards >= p2_top:
+            subgame_p1_cards = deque(islice(p1_cards, p1_top))
+            subgame_p2_cards = deque(islice(p2_cards, p2_top))
             winner = play_game(subgame_p1_cards, subgame_p2_cards)
         else:
             winner = 1 if p1_top > p2_top else 2
 
         if winner == 1:
             p1_cards.extend([p1_top, p2_top])
+            p1_num_cards += 2
         else:
             p2_cards.extend([p2_top, p1_top])
+            p2_num_cards += 2
 
     return winner
 
