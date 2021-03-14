eadd126071a54b325a9845bd5edae5c863f0cfb6
Abhijay Gupta <atg.abhijay@gmail.com>
2021-03-14 18:54:13 -0400

    Initial commit

---
+++ Day-24/advent-24.py
@@ -0,0 +1,25 @@
+"""
+URL for challenge: https://adventofcode.com/2020/day/24
+"""
+
+
+def part1():
+    return
+
+
+def part2():
+    return
+
+
+def run():
+    chall = int(input("Please enter either 1 or 2 for the challenges: "))
+    if chall == 1:
+        print(part1())
+    elif chall == 2:
+        print(part2())
+    else:
+        print("You need to enter either 1 or 2")
+        exit(1)
+
+
+run()
