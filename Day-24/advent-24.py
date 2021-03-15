eaddae02e6871e181dae499e06b09e4cc953bcb8
Abhijay Gupta <atg.abhijay@gmail.com>
2021-03-15 00:50:22 -0400

    Simplify processing of input

--- Day-24/advent-24.py
+++ Day-24/advent-24.py
@@ -1,41 +1,53 @@
 """
 URL for challenge: https://adventofcode.com/2020/day/24
+
+Check PR description for notes on solution.
 """
 
 
-from tqdm import tqdm
+from tqdm import trange
 
 
 def process_input():
     f = open("advent-24-input.txt")
-    all_directions = []
+    tiles = {(0, 0): 0}
+    increments = {'e': 2, 'w': -2, 'ne': (1, 1),
+                  'nw': (-1, 1), 'se': (1, -1), 'sw': (-1, -1)}
+
     for line in f.readlines():
-        line, directions = line.strip(), []
+        line = line.strip()
+        current_x, current_y = 0, 0
         step, is_north_south = '', False
         for char in line:
             if char in ['n', 's']:
                 step, is_north_south = char, True
             else:
                 if is_north_south:
-                    directions.append(step + char)
+                    incr = increments[step + char]
+                    current_x += incr[0]
+                    current_y += incr[1]
                     step, is_north_south = '', False
                 else:
-                    directions.append(char)
+                    current_x += increments[char]
 
-        all_directions.append(directions)
+        location = (current_x, current_y)
+        if location not in tiles:
+            tiles[location] = 1
+        else:
+            tiles[location] = 1 - tiles[location]
 
-    return all_directions
+    return tiles
 
 
 def part1():
-    return sum(map_out_floor().values())
+    return sum(process_input().values())
 
 
 def part2():
     # Takes about 10 seconds to run
-    tiles = map_out_floor()
+    tiles = process_input()
     tiles_to_flip, num_days = [], 100
-    for _ in tqdm(range(num_days)):
+    for _ in trange(num_days):
         tiles = expand_floor(tiles)
         for tile, color in tiles.items():
             num_black_nbs = check_neighbours(tiles, tile)
@@ -55,42 +67,10 @@ def part2():
     return sum(tiles.values())
 
 
-def map_out_floor():
-    all_directions = process_input()
-    tiles = {(0, 0): 0}
-    for directions in all_directions:
-        current_x, current_y = 0, 0
-        for step in directions:
-            if step == 'e':
-                current_x += 2
-            elif step == 'w':
-                current_x -= 2
-            elif step == 'se':
-                current_x += 1
-                current_y -= 1
-            elif step == 'sw':
-                current_x -= 1
-                current_y -= 1
-            elif step == 'ne':
-                current_x += 1
-                current_y += 1
-            else:
-                current_x -= 1
-                current_y += 1
-
-        location = (current_x, current_y)
-        if location not in tiles:
-            tiles[location] = 1
-        else:
-            tiles[location] = 1 - tiles[location]
-
-    return tiles
-
-
 def generate_neighbour_locations(target_tile):
     tile_x, tile_y = target_tile
     neigbhour_locations = [(tile_x + x, tile_y + y)
-                                for x in [-1, 1] for y in [-1, 1]]
+                           for x in [-1, 1] for y in [-1, 1]]
     neigbhour_locations.extend([(tile_x + x, tile_y) for x in [-2, 2]])
     return neigbhour_locations
 
@@ -99,9 +79,9 @@ def expand_floor(tiles):
     tiles_copy = tiles.copy()
     for tile in tiles:
         neighbour_locations = generate_neighbour_locations(tile)
-        for nb in neighbour_locations:
-            if nb not in tiles_copy:
-                tiles_copy[nb] = 0
+        for nbr in neighbour_locations:
+            if nbr not in tiles_copy:
+                tiles_copy[nbr] = 0
 
     return tiles_copy
 
@@ -109,9 +89,9 @@ def expand_floor(tiles):
 def check_neighbours(tiles, target_tile):
     num_black_nbs = 0
     neighbour_locations = generate_neighbour_locations(target_tile)
-    for nb in neighbour_locations:
-        if nb in tiles:
-            num_black_nbs += tiles[nb]
+    for nbr in neighbour_locations:
+        if nbr in tiles:
+            num_black_nbs += tiles[nbr]
 
     return num_black_nbs
 
