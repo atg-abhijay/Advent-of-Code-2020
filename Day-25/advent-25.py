1f40800e1ec9ef229e3c0e91c80ba87ba57363b7
Abhijay Gupta <atg.abhijay@gmail.com>
2021-04-26 10:48:39 -0400

    Process input separately

--- Day-25/advent-25.py
+++ Day-25/advent-25.py
@@ -3,6 +3,14 @@ URL for challenge: https://adventofcode.com/2020/day/25
 """
 
 
+def process_input():
+    f = open("advent-25-input.txt")
+    card_pubkey = int(f.readline().rstrip())
+    door_pubkey = int(f.readline().rstrip())
+
+    return card_pubkey, door_pubkey
+
+
 def part1():
     return
 
